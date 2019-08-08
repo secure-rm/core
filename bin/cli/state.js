7b95c3c79748a60fa2b1ab610d1c6aaa97ebe6aa
oganexon <oganexon@protonmail.com>
2019-08-08 19:50:49 +0200

    changed log again

--- bin/cli/state.js
+++ bin/cli/state.js
@@ -1,58 +1,14 @@
+const log = require('ololog').configure({ time: true, locate: false, tag: true })
 const chalk = require('chalk')
-var Multispinner = require('multispinner')
 const { eventEmitter } = require('../../lib/methods')
 
-var multispinner = new Multispinner(
-  { 'Start': 'Files analyzed' },
-  {
-    color: {
-      incomplete: 'cyan',
-      success: 'green',
-      error: 'red'
-    }
-  }
-)
-
-multispinner.success('Start')
-
-eventEmitter.on('writing', (file) => {
-  multispinner.spinners[file] = {
-    state: 'incomplete',
-    current: '',
-    text: chalk.bold.yellow('Writing ') + file
-  }
-})
-
-eventEmitter.on('deleting', (file) => {
-  multispinner.spinners[file] = {
-    state: 'incomplete',
-    current: '',
-    text: chalk.bold.magenta('Deleting ') + file
-  }
-})
-
-eventEmitter.on('info', (file, info) => {
-  multispinner.spinners[file] = {
-    state: 'incomplete',
-    current: '',
-    text: chalk.bold.blue(info) + file
-  }
-})
-
+eventEmitter.on('starting', (file) => log(chalk.bold.yellow('Starting ') + file))
+eventEmitter.on('deleting', (file) => log(chalk.bold.magenta('Deleting ') + file))
+eventEmitter.on('info', (file, info) => log(chalk.bold.blue(info) + file))
 eventEmitter.on('ending', (file) => {
-  multispinner.spinners[file].text = file
-  multispinner.success(file)
-  /* cli.action.start(chalk.green('Done ') + file)
-  cli.action.stop('') */
+  log(chalk.green('Done ') + file)
 })
 eventEmitter.on('error', (file, err) => {
-  multispinner.spinners[file] = {
-    state: 'error',
-    current: '',
-    text: file + ' ' + chalk.bold.red(err)
-  }
-  // multispinner.error(file)
-  /* cli.action.start(chalk.bold.red('Error ') + file)
-  cli.action.stop('')
-  cli.warn(chalk.yellow(err)) */
+  log.error(chalk.bold.red('Error ') + file)
+  log.warn(chalk.yellow(err))
 })
