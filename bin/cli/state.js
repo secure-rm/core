d957edc579a8cd7561beaad006f72afa34376c46
oganexon <oganexon@protonmail.com>
2019-08-09 15:04:33 +0200

    handle most errors with clearer messages

--- bin/cli/state.js
+++ bin/cli/state.js
@@ -3,13 +3,10 @@ const chalk = require('chalk')
 const { eventEmitter } = require('../../lib/methods')
 
 eventEmitter.on('starting', (file) => log(chalk.bold.yellow('Starting ') + file))
-eventEmitter.on('deleting', (file) => log(chalk.bold.magenta('Deleting ') + file))
-eventEmitter.on('info', (file, info) => log(chalk.bold.blue(info) + file))
-eventEmitter.on('ending', (file) => {
-  log(chalk.green('Done ') + file)
-})
+eventEmitter.on('unlinking', (file) => log(chalk.bold.magenta('Unlinking ') + file))
+eventEmitter.on('done', (file) => log(chalk.bold.green('Done ') + file))
+
+eventEmitter.on('info', (file, info) => log(chalk.blue(info) + file))
+
 eventEmitter.on('warn', (file, err) => log.warn(chalk.yellow(err) + file))
-eventEmitter.on('error', (file, err) => {
-  log.error(chalk.bold.red('Error ') + file)
-  log.warn(chalk.yellow(err))
-})
+eventEmitter.on('error', (file, err) => log.error(chalk.red(err) + file))
