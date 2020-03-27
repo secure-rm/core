5f273acd8e8de68054666024486ba2a3b999935e
oganexon <oganexon@protonmail.com>
2020-03-27 20:25:26 +0100

    added comments

--- src/dir.ts
+++ src/dir.ts
@@ -2,23 +2,20 @@ import fs from 'fs-extra'
 import path from 'path'
 import crypto from 'crypto'
 
-export async function rename (folderName: string, { passes = 1 }: Options = {}) {
+/**
+  * Rename the directory to a random string of length 12.
+  * @param folderName The folder that will be processed
+  */
+export async function rename (folderName: string) {
   const files = await fs.readdir(folderName)
   if (files.length) {
     // will throw error
     await fs.rmdir(folderName)
-    return ''
+    throw new Error('If this message appears, something went wrong')
   } else {
-    for (let i = 0; i < passes; i++) {
-      const newName = crypto.randomBytes(9).toString('base64').replace(/\//g, '0').replace(/\+/g, 'a')
-      const newPath = path.join(path.dirname(folderName), newName)
-      await fs.rename(folderName, newPath)
-      folderName = newPath
-    }
-    return folderName
+    const newName = crypto.randomBytes(9).toString('base64').replace(/\//g, '0').replace(/\+/g, 'a')
+    const newPath = path.join(path.dirname(folderName), newName)
+    await fs.rename(folderName, newPath)
+    return newPath
   }
-}
-
-interface Options {
-  passes?: number
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { remove } from './secure-rm'
-
 export default remove
+export { remove } from './remove'
 export * as fileMethods from './file'
 export * as dirMethods from './dir'
-export { remove } from './secure-rm'
 export { standards } from './standards'
